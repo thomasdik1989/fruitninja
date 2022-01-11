e7a30cb461b7b1bfd597f6699e0288b46f1f80e6
ThomasDik <thomasdik@live.nl>
2022-01-11 22:25:25 +0100

    Fixed object pooling. Added new model. Added force to fruit pieces.

--- Assets/FruitSpawner.cs
+++ Assets/FruitSpawner.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class FruitSpawner : MonoBehaviour {
   public GameObject fruitPrefab;
   public Transform[] spawnPoints;
+  private ObjectPool<GameObject> pool;
 
   public float minDelay = .1f;
   public float maxDelay = 1f;
 
   private void Start() {
+    pool = new ObjectPool<GameObject>(() => {
+      return Instantiate(fruitPrefab);
+    }, fruit => {
+      fruit.SetActive(true);
+      int spawnIndex = Random.Range(0, spawnPoints.Length);
+      Transform spawnPoint = spawnPoints[spawnIndex];
+      Transform transform = fruit.GetComponentInParent(typeof(Transform)) as Transform;
+      transform.SetPositionAndRotation(spawnPoint.position, spawnPoint.rotation);
+    }, fruit => {
+      fruit.SetActive(false);
+
+    }, fruit => {
+      Destroy(fruit, 5f);
+    }, false, 30, 100);
     StartCoroutine(SpawnFruits());
   }
 
@@ -17,11 +33,16 @@ public class FruitSpawner : MonoBehaviour {
       float delay = Random.Range(minDelay, maxDelay);
       yield return new WaitForSeconds(delay);
 
-      int spawnIndex = Random.Range(0, spawnPoints.Length);
-      Transform spawnPoint = spawnPoints[spawnIndex];
+      GameObject spawnedFruit = pool.Get();
+      StartCoroutine(DelayedHideGameObject(spawnedFruit, 4f));
+    }
+  }
 
-      GameObject spawnedFruit = Instantiate(fruitPrefab, spawnPoint.position, spawnPoint.rotation);
-      Destroy(spawnedFruit, 5f);
+  private IEnumerator DelayedHideGameObject(GameObject target, float time) {
+    yield return new WaitForSeconds(time);
+    if (target != null) {
+      target.SetActive(false);
+      pool.Release(target);
     }
   }
 
