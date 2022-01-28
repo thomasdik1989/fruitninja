259b68be30079cd82ab1c11284d69793e37f6160
ThomasDik <thomasdik@live.nl>
2022-01-29 00:23:49 +0100

    - Added a bomb model. - Introduced a destructable baseclass to reduce overhead. - Modified folder structure to reduce clutter. - Switched to HDRP. - Refactored all materials and shaders. - Added shaders and vfx for the bomb explosion animation.

--- Assets/Fruit.cs
+++ Assets/Fruit.cs
@@ -1,25 +1,10 @@
 using UnityEngine;
 
-public class Fruit : MonoBehaviour {
+public class Fruit : Destructable {
 
   public GameObject fruitSlicedPrefab;
   private GameObject fruitPieces;
   public GameObject ExplosionPrefab;
-  public ScoreManager scoreManager;
-
-  public float startForce = 12f;
-  public int points = 10;
-
-  private Rigidbody2D rb;
-
-  public void Awake() {
-    scoreManager = (ScoreManager)ScoreManager.Instance;
-  }
-
-  public void OnEnable() {
-    rb = GetComponent<Rigidbody2D>();
-    rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
-  }
 
   private void OnTriggerEnter2D(Collider2D collision) {
     if (collision.tag != "Blade") {
